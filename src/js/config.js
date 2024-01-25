5f53e0d7c66d3856ca441e722d50399d3cca83b0
MarioZeljko <mario.zeljko94mail.com>
2024-01-25 23:22:02 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+// For variables that are constants are that are to be reused accross the project
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
